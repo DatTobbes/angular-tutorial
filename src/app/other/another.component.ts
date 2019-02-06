eb0e5a7593f75ff3592bf8834a6cb52fc03313dc
DatTobbes <buttermilchschoppen@gmx.de>
2019-02-06 18:58:50 +0100

    add gitignore

---
+++ src/app/other/another.component.ts
@@ -0,0 +1,16 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-another',
+  template: `
+    <p>
+      another Works!
+    </p>
+    <hr>
+    <ng
+    
+  `,
+  styles: []
+})
+export class AnotherComponent {
+}
