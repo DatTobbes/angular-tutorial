39a83bf2286224dd99ee6ef42d18b77fd700cab5
Tobias-PC\Tobias <buttermilchschoppen@gmx.de>
2019-02-28 18:48:08 +0100

    project baseline

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,29 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { HeaderComponent } from './header.component';
+import {AppComponent} from './app.component';
+import { RecipeComponent } from './recipe/recipe.component';
 
-import { AppComponent } from './app.component';
-import { OtherComponent } from './other/other.component';
-import {AnotherComponent} from './other/another.component';
-import { DatabindingComponent } from './databinding/databinding.component';
-import { PropertyBindingComponent } from './databinding/property-binding.component';
-import { EventBindingComponent } from './databinding/event-binding.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    OtherComponent,
-    AnotherComponent,
-    DatabindingComponent,
-    PropertyBindingComponent,
-    EventBindingComponent
-  ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    RecipeComponent,
+  ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent ]
 })
 export class AppModule { }
