f383d1adcb78c9c232cf0553caa6ccb086558330
DatTobbes <buttermilchschoppen@gmx.de>
2019-03-25 17:07:10 +0100

    show ingredient list for each recipe

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import {Recipe} from '../recipe.model';
+import { Ingredient} from "../../shared/ingredient.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -10,8 +11,8 @@ export class RecipeListComponent implements OnInit {
   @Output() recipeSelected = new EventEmitter<Recipe>();
 
   recipes :Recipe[] = [
-    new Recipe('Dummy1', 'Dies ist ein dummy', 'http://guides.global/images/guides/global/dummy_web_page.jpg' ),
-    new Recipe('Dummy1', 'Dies ist ein dummy', 'http://guides.global/images/guides/global/dummy_web_page.jpg' )
+    new Recipe('Dummy1', 'Dies ist ein dummy', 'http://guides.global/images/guides/global/dummy_web_page.jpg', [new Ingredient("Pommes", 10), new Ingredient("Ketchup",1)] ),
+    new Recipe('Dummy2', 'Dies ist ein dummy', 'http://guides.global/images/guides/global/dummy_web_page.jpg', [new Ingredient("Pommes", 10), new Ingredient("Ketchup",1)] )
   ]
 
   selectedRecipe : Recipe;
