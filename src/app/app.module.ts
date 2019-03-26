3851f2d431a81c3dd45eea37a1c8416f8ddaf73a
DatTobbes <buttermilchschoppen@gmx.de>
2019-03-26 10:51:54 +0100

    add a drowdown with directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { HeaderComponent } from './header.component';
-import {AppComponent} from './app.component';
+import { AppComponent} from './app.component';
 import { RecipeComponent } from './recipe/recipe.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
 import { RecipeDetailsComponent } from './recipe/recipe-details/recipe-details.component';
 import { RecipeItemComponent } from './recipe/recipe-list/recipe-item.component';
 import { ShoppingListAddComponent } from './shopping-list/shopping-list-add.component';
+import { DropdownDirective} from './dropdown.directive';
 
 
 @NgModule({
@@ -27,6 +28,7 @@ import { ShoppingListAddComponent } from './shopping-list/shopping-list-add.comp
     RecipeDetailsComponent,
     RecipeItemComponent,
     ShoppingListAddComponent,
+    DropdownDirective
   ],
   providers: [],
   bootstrap: [AppComponent ]
