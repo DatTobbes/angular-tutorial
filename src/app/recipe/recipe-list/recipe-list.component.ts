c7b927756a396188060aa9b7c19b46350aa0b06d
DatTobbes <buttermilchschoppen@gmx.de>
2019-03-27 10:35:56 +0100

    add Recipe service

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import {Recipe} from '../recipe.model';
-import { Ingredient} from "../../shared/ingredient.model";
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -10,16 +10,14 @@ import { Ingredient} from "../../shared/ingredient.model";
 export class RecipeListComponent implements OnInit {
   @Output() recipeSelected = new EventEmitter<Recipe>();
 
-  recipes :Recipe[] = [
-    new Recipe('Dummy1', 'Dies ist ein dummy', 'http://guides.global/images/guides/global/dummy_web_page.jpg', [new Ingredient("Pommes", 10), new Ingredient("Ketchup",1)] ),
-    new Recipe('Dummy2', 'Dies ist ein dummy', 'http://guides.global/images/guides/global/dummy_web_page.jpg', [new Ingredient("Pommes", 10), new Ingredient("Ketchup",1)] )
-  ]
+  recipes :Recipe[];
 
   selectedRecipe : Recipe;
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onSelected(recipe:Recipe){
---
+++ src/app/recipe/recipe.service.ts
@@ -0,0 +1,16 @@
+import {Recipe} from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
+
+export class RecipeService{
+    private recipes: Recipe[] =  [
+        new Recipe('Dummy1', 'Dies ist ein dummy', 'http://guides.global/images/guides/global/dummy_web_page.jpg', 
+                    [new Ingredient("Pommes", 10), new Ingredient("Ketchup",1)] ),
+        new Recipe('Dummy2', 'Dies ist ein dummy', 'http://guides.global/images/guides/global/dummy_web_page.jpg',
+                    [new Ingredient("Pommes", 10), new Ingredient("Ketchup",1)] )
+    ];
+    
+    getRecipes() {
+          return this.recipes;
+    }
+    
+}
